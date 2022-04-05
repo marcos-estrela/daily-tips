bc17333233a0b1ed84c22f10a0cf95660c0bf7a4
Marcos Estrela <marcos@simples.vet>
2022-04-05 00:02:50 -0300

    Adiciona primeira versão teste para exibir webhook do usuário

---
+++ src/views/UserIdentification.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    {{ screenMessage }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "UserIdentification",
+
+  data() {
+    return {
+      oAuthAccessURL: "https://slack.com/api/oauth.v2.access",
+      slackAppScope: "chat:write:bot",
+      screenMessage: "",
+    };
+  },
+
+  computed: {
+    slackAuthHref() {
+      return `https://slack.com/oauth/v2/authorize?scope=${this.slackAppScope}&redirect_uri=${this.redirect_uri}&client_id=${this.client_id}`;
+    },
+  },
+
+  mounted() {
+    this.saveUserWebHook();
+  },
+
+  methods: {
+    async saveUserWebHook() {
+      const parameters = {
+        code: this.$route.query.code,
+        client_id: process.env.VUE_APP_CLIENT_ID,
+        client_secret: process.env.VUE_APP_CLIENT_SECRET,
+        redirect_uri: process.env.VUE_APP_REDIRECT_URI,
+      };
+      axios.get("https://slack.com/api/oauth.v2.access", parameters).then(
+        (response) => {
+          if (!response.incoming_webhook?.url) this.slackErrorHandler();
+
+          this.screenMessage = `Sucesso! Sua URL para webHook é ${response.incoming_webhook.url}`;
+        },
+        (error) => {
+          this.slackErrorHandler();
+          console.error(error);
+        }
+      );
+    },
+    slackErrorHandler() {
+      this.screenMessage =
+        "Ocorreu um erro ao tentar se conectar ao Slack. Não foi possível instalar o bot.";
+    },
+  },
+};
+</script>
